66bf3ec6eac058d34e387d1336d34d21e3f6afc7
Yassine Saadaoui <saadaouiy51@gmail.com>
2026-03-31 23:31:28 +0100

    update environment variables

--- src/commands/cv.seeder.ts
+++ src/commands/cv.seeder.ts
@@ -4,7 +4,7 @@ import { AppModule } from '../app.module';
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
 
-  // Todo : Do the seeding logic here
+  // TODO : Do the seeding logic here
 
   console.log(`Seed completed.`);
 
