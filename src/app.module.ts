49c3ca0a1e9b9f0209e1f236591c6982da03db6f
Yassine Saadaoui <saadaouiy51@gmail.com>
2026-03-31 23:15:11 +0100

    add resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { CvsModule } from './cvs/cvs.module';
+import { SkillModule } from './skill/skill.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule, CvsModule, SkillModule],
   controllers: [AppController],
   providers: [AppService],
 })
