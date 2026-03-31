640e43b3f37c38aadfce1c8a294f58752f0d0a0b
Yassine Saadaoui <saadaouiy51@gmail.com>
2026-03-31 23:42:37 +0100

    add DatabaseModule import to Cvs, Skill, and Users modules; implement lifecycle hooks in DatabaseService

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,12 +1,20 @@
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma/client';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 @Injectable()
-export class DatabaseService extends PrismaClient {
+export class DatabaseService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
     super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
--- src/skill/skill.module.ts
+++ src/skill/skill.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SkillService } from './skill.service';
 import { SkillController } from './skill.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [SkillController],
   providers: [SkillService],
 })
