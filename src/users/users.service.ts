3b68f69916532883d9a82d43626d7e0cf8dae210
Yassine Saadaoui <saadaouiy51@gmail.com>
2026-03-31 23:52:17 +0100

    refactor(users): replace DTOs with Prisma types in UsersController and UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,29 +1,35 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { DatabaseService } from 'src/database/database.service';
+import { Prisma } from 'generated/prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private databaseService: DatabaseService) {}
   
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  create(createUserDto: Prisma.UserCreateInput) {
+    return this.databaseService.user.create({ data: createUserDto });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.databaseService.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.databaseService.user.findUnique({
+      where: { id }
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: Prisma.UserUpdateInput) {
+    return this.databaseService.user.update({
+      where: { id },
+      data: updateUserDto
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.databaseService.user.delete({
+      where: { id }
+    });
   }
 }
