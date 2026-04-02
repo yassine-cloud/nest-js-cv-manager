613d88448b53df46d54d63ca8f4faf781f95ede1
Akram Zaabi <akramzaabi7@gmail.com>
2026-04-02 14:07:30 +0100

    added cv entity and DTO

--- src/cvs/dto/create-cv.dto.ts
+++ src/cvs/dto/create-cv.dto.ts
@@ -1,1 +1,36 @@
-export class CreateCvDto {}
+import { Type } from "class-transformer";
+import {
+    IsString,
+    Min,
+    IsOptional,
+    IsUUID,
+    IsInt,
+    Max,
+    IsArray,
+    ValidateNested
+} from "class-validator"
+import { CreateSkillDto } from "src/skill/dto/create-skill.dto";
+
+export class CreateCvDto {
+
+    @IsString()
+    firstName: string; 
+
+    @IsString()
+    name: string;
+    
+    @IsInt()
+    @Min(18)
+    @Max(90)
+    age: number
+    
+    @IsString()
+    Job: string;
+
+    @IsString()
+    @IsOptional()
+    path?: string;
+
+    @IsArray()
+    skills: { id: string, designation: string }[];
+}
--- src/cvs/entities/cv.entity.ts
+++ src/cvs/entities/cv.entity.ts
@@ -1,1 +1,13 @@
-export class Cv {}
+export class Cv {
+
+    id: string;
+    firstName: string;
+    name: string;
+    age: number;
+    Cin: string;
+    Job: string;
+    path: string | null;
+    userId: string;
+    user?: { id: string, firstName: string, name: string, email: string };
+    skills?: { id: string, designation: string }[];
+}
